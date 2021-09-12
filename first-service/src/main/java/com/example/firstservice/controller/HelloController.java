c816152b1082aad124abb2941445b9f4d1e6d02d
lukelee <lukelee01@gmail.com>
2021-09-11 22:26:07 -0400

    add Custom, Global, and LoggingFilter

--- first-service/src/main/java/com/example/firstservice/controller/HelloController.java
+++ first-service/src/main/java/com/example/firstservice/controller/HelloController.java
@@ -1,17 +1,31 @@
 package com.example.firstservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 public class HelloController {
 
     @GetMapping("/welcome")
     public ResponseEntity<String> getHello() {
         return new ResponseEntity<>("Hello from first-service", HttpStatus.OK);
+    }
+
+    @GetMapping("/message")
+    public ResponseEntity<String> getMessage(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return new ResponseEntity<>("Hello World in First Service", HttpStatus.OK);
+    }
+
+    @GetMapping("/check")
+    public ResponseEntity<String> getCheck() {
+        return new ResponseEntity<>("This is from first-service!!!", HttpStatus.OK);
     }
 }
--- second-service/src/main/java/com/example/secondservice/controller/HelloController.java
+++ second-service/src/main/java/com/example/secondservice/controller/HelloController.java
@@ -1,17 +1,31 @@
 package com.example.secondservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/second-service")
+@RequestMapping("/second-service")
+@Slf4j
 public class HelloController {
 
     @GetMapping("/welcome")
     public ResponseEntity<String> getHello() {
         return new ResponseEntity<>("Hello From Second-service", HttpStatus.OK);
+    }
+
+    @GetMapping("/message")
+    public ResponseEntity<String> getMessage(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return new ResponseEntity<>("Hello World in Second Service", HttpStatus.OK);
+    }
+
+    @GetMapping("/check")
+    public ResponseEntity<String> getCheck() {
+        return new ResponseEntity<>("This is from second-service!!!", HttpStatus.OK);
     }
 }
