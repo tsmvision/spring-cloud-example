c73dd1cb9f9cb440b1a09ff8e65b3ba1053b3b29
lukelee <lukelee01@gmail.com>
2021-09-11 23:48:02 -0400

    update first, second service

--- first-service/src/main/java/com/example/firstservice/controller/HelloController.java
+++ first-service/src/main/java/com/example/firstservice/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.firstservice.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +9,15 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
 public class HelloController {
+
+    @Value("${server.port}")
+    private String portValue;
 
     @GetMapping("/welcome")
     public ResponseEntity<String> getHello() {
@@ -25,7 +31,9 @@ public class HelloController {
     }
 
     @GetMapping("/check")
-    public ResponseEntity<String> getCheck() {
-        return new ResponseEntity<>("This is from first-service!!!", HttpStatus.OK);
+    public ResponseEntity<String> getCheck(HttpServletRequest request) {
+
+        log.info("Server port={}", request.getServerPort());
+        return new ResponseEntity<>(String.format("First Service on PoRT %s", portValue), HttpStatus.OK);
     }
 }
